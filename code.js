c5a4ba9fba303c87b0f035511ee2e8a51e595ff8
Djhorsten <d.horsten@sintlucasedu.nl>
2024-12-13 10:31:47 +0100

    TF2 Memes for you
    Enjoy some fresh tf2 memes while you laugh.

---
+++ code.js
@@ -0,0 +1,22 @@
+let slideIndex = 0;
+showSlides();
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n >= slides.length) { slideIndex = 0 }
+    if (n < 0) { slideIndex = slides.length - 1 }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
+
+// Optional: Auto slide every 3 seconds
+setInterval(() => {
+    plusSlides(1);
+}, 10000);
